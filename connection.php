054930eef35c00b279b82c84b9a1a22a19106309
simakovich77 <simakovich77@gmail.com>
2017-02-27 03:41:56 +0300

    dfsdfsdf

---
+++ connection.php
@@ -0,0 +1,8 @@
+<?php
+$host = '194.135.91.242'; // адрес сервера
+$database = 'common_vanobit'; // имя базы данных
+$user = 'common_vanobit'; // имя пользователя
+$password = '0xHfxZPUFI'; // пароль
+?>
+
+
